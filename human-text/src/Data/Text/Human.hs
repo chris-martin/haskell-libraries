24755fcae4211a77225fabcaa7bddc43ecc3bfbc
Chris Martin <ch.martin@gmail.com>
2017-04-07 18:30:00 -0400

    human-text: add Either instance

--- human-text/src/Data/Text/Human.hs
+++ human-text/src/Data/Text/Human.hs
@@ -2,6 +2,7 @@ module Data.Text.Human
     ( HumanText (..)
     ) where
 
+import Data.Either (Either (..))
 import Data.Text (Text)
 
 -- | An entirely unprincipled class for things which can be converted to text
@@ -10,3 +11,7 @@ import Data.Text (Text)
 class HumanText a where
 
     humanText :: a -> Text
+
+instance (HumanText a, HumanText b) => HumanText (Either a b) where
+    humanText (Left  a) = humanText a
+    humanText (Right b) = humanText b
