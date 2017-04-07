838482b5ba5db33ab47fd0b7e8361fce2bc18e15
Chris Martin <ch.martin@gmail.com>
2017-04-07 19:02:01 -0400

    human-text: add Void instance

--- human-text/src/Data/Text/Human.hs
+++ human-text/src/Data/Text/Human.hs
@@ -4,6 +4,7 @@ module Data.Text.Human
 
 import Data.Either (Either (..))
 import Data.Text (Text)
+import Data.Void (Void, absurd)
 
 -- | An entirely unprincipled class for things which can be converted to text
 -- that might be suitable to show to a human user. The output should probably
@@ -11,6 +12,9 @@ import Data.Text (Text)
 class HumanText a where
 
     humanText :: a -> Text
+
+instance HumanText Void where
+    humanText = absurd
 
 instance (HumanText a, HumanText b) => HumanText (Either a b) where
     humanText (Left  a) = humanText a
