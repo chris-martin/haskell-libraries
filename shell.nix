9bc4cfff9d1c088de42498ff83c3522e2af20b0e
Chris Martin <ch.martin@gmail.com>
2017-05-18 00:58:47 -0400

    clock-app: init

--- shell.nix
+++ shell.nix
@@ -5,7 +5,17 @@ with pkgs;
 haskell.lib.buildStackProject {
   name = "my-haskell-libraries";
   inherit ghc;
-  buildInputs = [ zlib ];
+  buildInputs = [
+    cairo
+    glib
+    gtk3
+    pango
+    pkgconfig
+    xorg.libX11
+    xorg.libXrandr
+    xorg.libXext
+    zlib
+  ];
 
   # https://github.com/commercialhaskell/stack/commit/568938da
   TMPDIR = "/tmp";
