94a970e19f82e301b00b3f600c83043a1443ecf1
Chris Martin <ch.martin@gmail.com>
2017-07-20 11:15:19 -0400

    alternating-characters: bugfix

--- alternating-characters/src/AlternatingCharacters.hs
+++ alternating-characters/src/AlternatingCharacters.hs
@@ -8,6 +8,9 @@ https://stackoverflow.com/questions/45203121
 >>> solve "a"
 1
 
+>>> solve "aa"
+0
+
 >>> solve "ab"
 2
 
@@ -17,7 +20,13 @@ https://stackoverflow.com/questions/45203121
 >>> solve "aba"
 3
 
+>>> solve "abba"
+0
+
 >>> solve "abca"
+3
+
+>>> solve "abcba"
 3
 
 >>> solve "beabeefeab"
@@ -31,10 +40,13 @@ https://stackoverflow.com/questions/45203121
 
 -}
 
+{-# LANGUAGE MultiWayIf #-}
+
 module AlternatingCharacters
   ( solve
   ) where
 
+import Data.Bool (otherwise)
 import Data.Eq (Eq (..))
 import Numeric.Natural (Natural)
 import Prelude (max, filter, succ)
@@ -46,10 +58,10 @@ import Prelude (max, filter, succ)
 -}
 solve :: Eq a => [a] -> Natural
 solve [] = 0
-solve (x:xs) =
-  max
-    (solve (filter (/= x) xs))
-    (solve' x xs)
+solve (x:xs) = max s s'
+  where
+    s = solve (filter (/= x) xs)
+    s' = solve' x xs
 
 {- |
 
@@ -60,9 +72,11 @@ alternation starts with @a@.
 solve' :: Eq a => a -> [a] -> Natural
 solve' _ [] = 1
 solve' a (x:xs) =
-  max
-    (solve' a (filter (/= x) xs))
-    (solve'' 2 a x xs)
+  if | x == a    -> 0
+     | otherwise -> max s' s''
+  where
+    s' = solve' a (filter (/= x) xs)
+    s'' = solve'' 2 a x xs
 
 {- |
 
@@ -74,6 +88,6 @@ alternation contains the letters @a@ and @b@.
 solve'' :: Eq a => Natural -> a -> a -> [a] -> Natural
 solve'' n _ _ [] = n
 solve'' n a b (x:xs) =
-  if x == a
-    then solve'' (succ 1) b a xs
-    else solve'' n a b xs
+  if | x == a    -> solve'' (succ n) b a xs
+     | x == b    -> 0
+     | otherwise -> solve'' n a b xs
